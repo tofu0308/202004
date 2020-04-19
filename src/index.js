736239b5aa694adbaa911b69487cafc9e6ad5ff8
tofu0308 <isisaka2000@gmail.com>
2020-04-19 18:42:21 +0900

    加算ボタンの動作

--- src/index.js
+++ src/index.js
@@ -15,14 +15,49 @@ class Container extends  React.Component {
     return (
       <div>
         <Welcome />
-        <button>
+
+        <button onClick = {e => {
+          // ReducerにActionをdispatchする
+          this.props.dispatch({type: "INCREMENT"});
+          console.log('INCREMENT');
+        }}>
           加算ボタン
         </button>
 
-        <p>ここに数値が入る</p>
+      <p>{this.props.value}</p>
       </div>
     )
   }
 }
 
-render (<Container />,document.getElementById("root"));
+// NewComponent = connect(ComponentからdispatchされたAction) (Component)
+const App = connect(
+  state => state
+)(Container);
+
+// Reducer
+const reducer = (state = {value: 0}, action) => {
+    // Componentの中でdispatchされたActionがaction変数に入ってくる
+    // action = { type: "INCREMENT" }
+    switch(action.type) {
+      case "INCREMENT":
+        // valueを加算
+        return Object.assign({}, {value: state.value + 1});
+      default:
+        return state;
+    }
+}
+
+// Reducerの戻り値を新しい状態（State）としてStoreで管理する
+const store = createStore(reducer);
+
+// Root Render
+// ReduxのProviderコンポーネントで、Appコンポーネント
+// （connect関数にComponentを渡して作成したNewComponent）をラップし、Storeを渡す
+// （簡単に言えば）propsにStateが入る
+render (
+  <Provider store={ store }>
+      <App />
+  </Provider>,
+  document.getElementById("root")
+);
