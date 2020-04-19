797a151211797c55f5eb8e230faae1194813b263
tofu0308 <isisaka2000@gmail.com>
2020-04-19 18:26:18 +0900

    側を作る

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from "react";
 import { render } from "react-dom";
+import { createStore } from "redux";
+import { Provider, connect } from "react-redux";
 
 class Welcome extends React.Component {
   render() {
     return <h1>Hello, World</h1>;
   }
 }
-render (<Welcome />,document.getElementById("root"));
+
+
+class Container extends  React.Component {
+  render() {
+    return (
+      <div>
+        <Welcome />
+        <button>
+          加算ボタン
+        </button>
+
+        <p>ここに数値が入る</p>
+      </div>
+    )
+  }
+}
+
+render (<Container />,document.getElementById("root"));
