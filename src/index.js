517687aa2436f6b2364d131ab0542b50640454ea
tofu0308 <isisaka2000@gmail.com>
2020-04-20 20:45:54 +0900

    todo app作り始め。Reducer

--- src/index.js
+++ src/index.js
@@ -21,6 +21,19 @@ export const addTodo = (text) => {
   }
 }
 
+// reducer
+const todo = (state, action) => {
+  switch(action.type) {
+    case 'ADD_TODO':
+      return {
+        id: action.id,
+        text: action.text
+      }
+      default: return state
+  }
+}
+export default todo
+
 
 
 class Welcome extends React.Component {
