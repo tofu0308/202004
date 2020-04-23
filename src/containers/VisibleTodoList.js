f3aaa8ffd2bb4afbd4449a2d491d89fb8606072c
tofu0308 <isisaka2000@gmail.com>
2020-04-23 22:31:02 +0900

    検証用console

--- src/containers/VisibleTodoList.js
+++ src/containers/VisibleTodoList.js
@@ -2,6 +2,8 @@ import { connect } from 'react-redux'
 import TodoList from '../components/TodoList'
 
 const mapStateToProps = (state) => {
+  console.log('mapStateToProps');
+  console.log(state);
   return { todos: state.todos }
 }
 
